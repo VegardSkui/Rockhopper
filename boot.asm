743eb3dce5d904ec6f0294161fa48f64f0f622ee
Vegard Skui <me@vegardskui.com>
2020-05-07 17:42:40 +0200

    Clear direction flag and use local labels

--- boot.asm
+++ boot.asm
@@ -1,24 +1,28 @@
-[ORG 0x7C00]
+ORG 0x7C00
+BITS 16
+
     xor ax, ax
     mov ds, ax
 
     mov si, greeting
-    call sprint
+    call bios_print
 
 hang:
+    hlt
     jmp hang
 
 ; Print string from SI using the BIOS
-sprint:
+bios_print:
+    cld             ; Clear direction flag (to make sure SI is incremented)
+.loop
     lodsb           ; Load byte (character) at address DS:SI into AL
     or al, al       ; Zero means we're at the end of the string
-    jz done
+    jz .done
     mov ah, 0x0E    ; Select teletype output function (write character)
     mov bh, 0       ; Page zero
     int 0x10        ; BIOS Interrupt
-    jmp sprint      ; Repeat for the next character
-
-done:
+    jmp .loop       ; Repeat for the next character
+.done:
     ret
 
 greeting db "Hello World!", 13, 10, "- Rockhopper", 13, 10, 0
