2a2c59cc64169b1427551873da9c0b5635c27782
Vegard Skui <me@vegardskui.com>
2021-01-05 13:34:05 +0100

    Add all warning codes from the spec

--- uefi-rust/libs/rk_uefi/src/data_types/common.rs
+++ uefi-rust/libs/rk_uefi/src/data_types/common.rs
@@ -29,6 +29,13 @@ impl EfiStatus {
     pub const EFI_SUCCESS: EfiStatus = EfiStatus(0);
 
     // Warning Codes
+    pub const EFI_WARN_UNKNOWN_GLYPH: EfiStatus = EfiStatus(1);
+    pub const EFI_WARN_DELETE_FAILURE: EfiStatus = EfiStatus(2);
+    pub const EFI_WARN_WRITE_FAILURE: EfiStatus = EfiStatus(3);
+    pub const EFI_WARN_BUFFER_TOO_SMALL: EfiStatus = EfiStatus(4);
+    pub const EFI_WARN_STALE_DATA: EfiStatus = EfiStatus(5);
+    pub const EFI_WARN_FILE_SYSTEM: EfiStatus = EfiStatus(6);
+    pub const EFI_WARN_RESET_REQUIRED: EfiStatus = EfiStatus(7);
 
     // Error Codes
     pub const EFI_INVALID_PARAMETER: EfiStatus = EfiStatus(Self::ERROR_BIT | 2);
@@ -39,13 +46,24 @@ impl EfiStatus {
 impl fmt::Debug for EfiStatus {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
+            // Success Codes
             Self::EFI_SUCCESS => write!(f, "EFI_SUCCESS"),
 
+            // Warning Codes
+            Self::EFI_WARN_UNKNOWN_GLYPH => write!(f, "EFI_WARN_UNKNOWN_GLYPH"),
+            Self::EFI_WARN_DELETE_FAILURE => write!(f, "EFI_WARN_DELETE_FAILURE"),
+            Self::EFI_WARN_WRITE_FAILURE => write!(f, "EFI_WARN_WRITE_FAILURE"),
+            Self::EFI_WARN_BUFFER_TOO_SMALL => write!(f, "EFI_WARN_BUFFER_TOO_SMALL"),
+            Self::EFI_WARN_STALE_DATA => write!(f, "EFI_WARN_STALE_DATA"),
+            Self::EFI_WARN_FILE_SYSTEM => write!(f, "EFI_WARN_FILE_SYSTEM"),
+            Self::EFI_WARN_RESET_REQUIRED => write!(f, "EFI_WARN_RESET_REQUIRED"),
+
+            // Error Codes
             Self::EFI_INVALID_PARAMETER => write!(f, "EFI_INVALID_PARAMETER"),
             Self::EFI_BUFFER_TOO_SMALL => write!(f, "EFI_BUFFER_TOO_SMALL"),
             Self::EFI_OUT_OF_RESOURCES => write!(f, "EFI_OUT_OF_RESOURCES"),
 
-            // Catch unknown status codes
+            // Unknown Codes
             Self(unknown) => write!(f, "EfiStatus({})", unknown),
         }
     }
