7dcb60c9a130c8b1122c4664f7c29623c7102e89
Vegard Skui <me@vegardskui.com>
2021-01-06 10:10:03 +0100

    Update OVMF version

--- uefi-rust/qemu.sh
+++ uefi-rust/qemu.sh
@@ -6,7 +6,8 @@ cp target/x86_64-unknown-uefi/debug/uefi-rust.efi disk/EFI/BOOT/BOOTX64.EFI
 qemu-system-x86_64 \
     -nodefaults \
     -cpu qemu64 \
-    -bios bios64.bin \
+    -drive if=pflash,format=raw,file=OVMF_CODE-pure-efi.fd,readonly=on \
+    -drive if=pflash,format=raw,file=OVMF_VARS-pure-efi.fd,readonly=on \
     -drive format=raw,file=fat:rw:disk \
     -serial stdio \
     -vga std
