984f6faf2b90a3b810c94515632a527d2bdcc723
Vegard Skui <me@vegardskui.com>
2021-01-07 18:23:58 +0100

    Read and write CR0 and CR4

--- uefi-rust/libs/rk_x86_64/src/register.rs
+++ uefi-rust/libs/rk_x86_64/src/register.rs
@@ -1,3 +1,21 @@
+pub mod cr0 {
+    /// Returns the current value of the CR0 register.
+    pub fn read() -> u64 {
+        let value: u64;
+        unsafe {
+            asm!("mov {}, cr0", out(reg) value);
+        }
+        value
+    }
+
+    /// Loads a new value into the CR0 register.
+    pub fn write(value: u64) {
+        unsafe {
+            asm!("mov cr0, {}", in(reg) value);
+        }
+    }
+}
+
 pub mod cr3 {
     /// Returns the current value of the CR3 register.
     pub fn read() -> u64 {
@@ -12,6 +30,24 @@ pub mod cr3 {
     pub fn write(value: u64) {
         unsafe {
             asm!("mov cr3, {}", in(reg) value);
+        }
+    }
+}
+
+pub mod cr4 {
+    /// Returns the current value of the CR4 register.
+    pub fn read() -> u64 {
+        let value: u64;
+        unsafe {
+            asm!("mov {}, cr4", out(reg) value);
+        }
+        value
+    }
+
+    /// Loads a new value into the CR4 register.
+    pub fn write(value: u64) {
+        unsafe {
+            asm!("mov cr4, {}", in(reg) value);
         }
     }
 }
