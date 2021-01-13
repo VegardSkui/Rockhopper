16b6ecef3506f2e5fcc3a8da260dd8517cad06c6
Vegard Skui <me@vegardskui.com>
2021-01-13 22:57:56 +0100

    System table doesn't need to be mutable

--- libs/rk_uefi/src/lib.rs
+++ libs/rk_uefi/src/lib.rs
@@ -12,15 +12,19 @@ use crate::data_types::EfiHandle;
 use crate::table::EfiSystemTable;
 
 static mut IMAGE_HANDLE: EfiHandle = EfiHandle(core::ptr::null());
-static mut SYSTEM_TABLE: *mut EfiSystemTable = 0 as *mut EfiSystemTable;
+static mut SYSTEM_TABLE: *const EfiSystemTable = 0 as *const EfiSystemTable;
 
-pub fn init(image_handle: EfiHandle, system_table: &'static mut EfiSystemTable) {
+/// Initializes the UEFI library with the given image handle and system table.
+///
+/// Should be called before any other function in the library.
+pub fn init(image_handle: EfiHandle, system_table: &'static EfiSystemTable) {
     unsafe {
         IMAGE_HANDLE = image_handle;
         SYSTEM_TABLE = system_table;
     }
 }
 
+/// Returns a reference to the current system table.
 pub fn system_table() -> &'static EfiSystemTable {
     // Safety: We assume the system table has been set
     unsafe { &*SYSTEM_TABLE }
